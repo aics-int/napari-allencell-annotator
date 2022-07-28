d881bc0046e0db9bc4021c49ede896f2888c56d1
Beatrice Bridge <beatricembridge@gmail.com>
2022-07-28 11:43:48 -0700

    lint

--- napari_allencell_annotator/__init__.py
+++ napari_allencell_annotator/__init__.py
@@ -1,11 +1,9 @@
 from napari_allencell_annotator.controller.main_controller import MainController
 
-
 try:
     from ._version import version as __version__
 except ImportError:
     __version__ = "unknown"
-
 
 # Do not edit this string manually, always use bumpversion
 # Details in CONTRIBUTING.md
@@ -13,4 +11,4 @@ __version__ = "0.0.2"
 
 
 def make_widget():
-    return MainController()
+    return MainController
