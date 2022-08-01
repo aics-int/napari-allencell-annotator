698b529fcaa79444df5d876cb2a12256a2165f26
Beatrice Bridge <beatricembridge@gmail.com>
2022-08-01 14:42:43 -0700

    annotator view tests

--- napari_allencell_annotator/_tests/view/annotator_view_test.py
+++ napari_allencell_annotator/_tests/view/annotator_view_test.py
@@ -1,7 +1,19 @@
 from unittest import mock
-from unittest.mock import MagicMock
+from unittest.mock import MagicMock, create_autospec
 
-from napari_allencell_annotator.view.annotator_view import AnnotatorView, AnnotatorViewMode, QListWidget
+from napari_allencell_annotator.view.annotator_view import (
+    AnnotatorView,
+    AnnotatorViewMode,
+    QWidget,
+    QListWidget,
+    QLineEdit,
+    QSpinBox,
+    QCheckBox,
+    QComboBox,
+    QVBoxLayout,
+    QListWidgetItem,
+    QLabel,
+)
 
 
 class TestAnnotatorView:
@@ -54,3 +66,238 @@ class TestAnnotatorView:
         self._view.render_values = MagicMock()
         self._view.render_default_values()
         self._view.render_values.assert_called_once_with([])
+
+    def test_render_values_line_input(self):
+        q_line_edit = create_autospec(QLineEdit)
+        self._view.annotation_item_widgets = [q_line_edit]
+        self._view.default_vals = ["default"]
+        self._view.render_values(["hello"])
+        q_line_edit.setText.assert_called_once_with("hello")
+
+    def test_render_values_line_no_input(self):
+        q_line_edit = create_autospec(QLineEdit)
+        self._view.annotation_item_widgets = [q_line_edit]
+        self._view.default_vals = ["default"]
+        self._view.render_values([""])
+        q_line_edit.setText.assert_called_once_with("default")
+
+    def test_render_values_line_none_input(self):
+        q_line_edit = create_autospec(QLineEdit)
+        self._view.annotation_item_widgets = [q_line_edit]
+        self._view.default_vals = ["default"]
+        self._view.render_values([None])
+        q_line_edit.setText.assert_called_once_with("default")
+
+    def test_render_values_spin(self):
+        q_line_edit = create_autospec(QLineEdit)
+        q_spin = create_autospec(QSpinBox)
+        self._view.annotation_item_widgets = [q_spin]
+        self._view.default_vals = [8]
+        self._view.render_values(["0"])
+        q_spin.setValue.assert_called_once_with(0)
+
+    def test_render_values_spin_empty(self):
+        q_line_edit = create_autospec(QLineEdit)
+        q_spin = create_autospec(QSpinBox)
+        self._view.annotation_item_widgets = [q_spin]
+        self._view.default_vals = ["8"]
+        self._view.render_values([""])
+        q_spin.setValue.assert_called_once_with(8)
+
+    def test_render_values_check(self):
+        q_line_edit = create_autospec(QLineEdit)
+        q_spin = create_autospec(QSpinBox)
+        q_check = create_autospec(QCheckBox)
+        self._view.annotation_item_widgets = [q_check]
+        self._view.default_vals = [True]
+        self._view.render_values(["False"])
+        q_check.setChecked.assert_called_once_with(False)
+
+    def test_render_values_check_empty(self):
+        q_line_edit = create_autospec(QLineEdit)
+        q_spin = create_autospec(QSpinBox)
+        q_check = create_autospec(QCheckBox)
+        self._view.annotation_item_widgets = [q_check]
+        self._view.default_vals = [True]
+        self._view.render_values([""])
+        q_check.setChecked.assert_called_once_with(True)
+
+    def test_render_values_combo(self):
+        q_line_edit = create_autospec(QLineEdit)
+        q_spin = create_autospec(QSpinBox)
+        q_check = create_autospec(QCheckBox)
+        q_combo = create_autospec(QComboBox)
+        self._view.annotation_item_widgets = [q_combo]
+        self._view.default_vals = ["option1"]
+        self._view.render_values(["option2"])
+        q_combo.setCurrentText.assert_called_once_with("option2")
+
+    def test_render_values_combo_empty(self):
+        q_line_edit = create_autospec(QLineEdit)
+        q_spin = create_autospec(QSpinBox)
+        q_check = create_autospec(QCheckBox)
+        q_combo = create_autospec(QComboBox)
+        self._view.annotation_item_widgets = [q_combo]
+        self._view.default_vals = ["option1"]
+        self._view.render_values([""])
+        q_combo.setCurrentText.assert_called_once_with("option1")
+
+    def test_render_values_mult_empty(self):
+        q_line_edit = create_autospec(QLineEdit)
+        q_spin = create_autospec(QSpinBox)
+        q_check = create_autospec(QCheckBox)
+        q_combo = create_autospec(QComboBox)
+        self._view.annotation_item_widgets = [q_line_edit, q_spin, q_check, q_combo]
+        self._view.default_vals = ["default", "0", True, "option1"]
+        self._view.render_values(["", "", "", ""])
+        q_line_edit.setText.assert_called_once_with("default")
+        q_spin.setValue.assert_called_once_with(0)
+        q_check.setChecked.assert_called_once_with(True)
+        q_combo.setCurrentText.assert_called_once_with("option1")
+
+    def test_render_values_mult_some_empty(self):
+        q_line_edit = create_autospec(QLineEdit)
+        q_spin = create_autospec(QSpinBox)
+        q_check = create_autospec(QCheckBox)
+        q_combo = create_autospec(QComboBox)
+        self._view.annotation_item_widgets = [q_line_edit, q_spin, q_check, q_combo]
+        self._view.default_vals = ["default", "0", True, "option1"]
+        self._view.render_values(["", "5", "", "option2"])
+        q_line_edit.setText.assert_called_once_with("default")
+        q_spin.setValue.assert_called_once_with(5)
+        q_check.setChecked.assert_called_once_with(True)
+        q_combo.setCurrentText.assert_called_once_with("option2")
+
+    def test_render_values_mult_some_none_some_empty(self):
+        q_line_edit = create_autospec(QLineEdit)
+        q_spin = create_autospec(QSpinBox)
+        q_check = create_autospec(QCheckBox)
+        q_combo = create_autospec(QComboBox)
+        self._view.annotation_item_widgets = [q_line_edit, q_spin, q_check, q_combo]
+        self._view.default_vals = ["default", "0", True, "option1"]
+        self._view.render_values([None, "", "False", ""])
+        q_line_edit.setText.assert_called_once_with("default")
+        q_spin.setValue.assert_called_once_with(0)
+        q_check.setChecked.assert_called_once_with(False)
+        q_combo.setCurrentText.assert_called_once_with("option1")
+
+    def test_get_curr_annots(self):
+        q_line_edit = create_autospec(QLineEdit)
+        q_line_edit.text = MagicMock(return_value="text")
+        q_spin = create_autospec(QSpinBox)
+        q_spin.value = MagicMock(return_value=5)
+        q_check = create_autospec(QCheckBox)
+        q_check.isChecked = MagicMock(return_value=True)
+        q_combo = create_autospec(QComboBox)
+        q_combo.currentText = MagicMock(return_value="text2")
+        self._view.annotation_item_widgets = [q_line_edit, q_spin, q_check, q_combo]
+        assert self._view.get_curr_annots() == ["text", 5, True, "text2"]
+
+    def test_toggle_annots_editable_true(self):
+        q_line_edit = create_autospec(QLineEdit)
+        q_spin = create_autospec(QSpinBox)
+        q_check = create_autospec(QCheckBox)
+        q_combo = create_autospec(QComboBox)
+        self._view.annotation_item_widgets = [q_line_edit, q_spin, q_check, q_combo]
+        self._view.toggle_annots_editable(True)
+        q_line_edit.setEnabled.assert_called_once_with(True)
+        q_spin.setEnabled.assert_called_once_with(True)
+        q_check.setEnabled.assert_called_once_with(True)
+        q_combo.setEnabled.assert_called_once_with(True)
+
+    def test_toggle_annots_editable_false(self):
+        q_line_edit = create_autospec(QLineEdit)
+        q_spin = create_autospec(QSpinBox)
+        q_check = create_autospec(QCheckBox)
+        q_combo = create_autospec(QComboBox)
+        self._view.annotation_item_widgets = [q_line_edit, q_spin, q_check, q_combo]
+        self._view.toggle_annots_editable(False)
+        q_line_edit.setEnabled.assert_called_once_with(False)
+        q_spin.setEnabled.assert_called_once_with(False)
+        q_check.setEnabled.assert_called_once_with(False)
+        q_combo.setEnabled.assert_called_once_with(False)
+
+    def test_display_mode_add_mode(self):
+        self._view._mode = AnnotatorViewMode.ADD
+        self._view.add_widget = MagicMock()
+        self._view._reset_annotations = MagicMock()
+        self._view.layout = create_autospec(QVBoxLayout)
+        self._view.layout.count = MagicMock(return_value=3)
+        item = create_autospec(QListWidgetItem)
+        widget = create_autospec(QLineEdit)
+        item.widget = MagicMock(return_value=widget)
+        self._view.layout.itemAt = MagicMock(return_value=item)
+
+        self._view._display_mode()
+
+        self._view.layout.count.assert_called_once_with()
+        item.widget.assert_called_once_with()
+        widget.hide.assert_called_once_with()
+        self._view.layout.removeItem.assert_called_once_with(item)
+        self._view.add_widget.show.assert_called_once_with()
+        self._view._reset_annotations.assert_called_once_with()
+        self._view.layout.addWidget.assert_called_once_with(self._view.add_widget)
+
+    def test_display_mode_view_mode(self):
+        self._view._mode = AnnotatorViewMode.VIEW
+        self._view.save_json_btn = MagicMock()
+        self._view.view_widget = MagicMock()
+        self._view.layout = create_autospec(QVBoxLayout)
+        self._view.layout.count = MagicMock(return_value=3)
+        item = create_autospec(QListWidgetItem)
+        widget = create_autospec(QLineEdit)
+        item.widget = MagicMock(return_value=widget)
+        self._view.layout.itemAt = MagicMock(return_value=item)
+
+        self._view._display_mode()
+
+        self._view.layout.count.assert_called_once_with()
+        item.widget.assert_called_once_with()
+        widget.hide.assert_called_once_with()
+        self._view.layout.removeItem.assert_called_once_with(item)
+        self._view.view_widget.show.assert_called_once_with()
+        self._view.save_json_btn.setEnabled.assert_called_once_with(True)
+        self._view.layout.addWidget.assert_called_once_with(self._view.view_widget)
+
+    def test_display_mode_annotate_mode(self):
+        self._view._mode = AnnotatorViewMode.ANNOTATE
+        self._view.prev_btn = MagicMock()
+        self._view.annot_widget = MagicMock()
+        self._view.layout = create_autospec(QVBoxLayout)
+        self._view.layout.count = MagicMock(return_value=3)
+        item = create_autospec(QListWidgetItem)
+        widget = create_autospec(QLineEdit)
+        item.widget = MagicMock(return_value=widget)
+        self._view.layout.itemAt = MagicMock(return_value=item)
+
+        self._view._display_mode()
+
+        self._view.layout.count.assert_called_once_with()
+        item.widget.assert_called_once_with()
+        widget.hide.assert_called_once_with()
+        self._view.layout.removeItem.assert_called_once_with(item)
+        self._view.annot_widget.show.assert_called_once_with()
+        self._view.prev_btn.setEnabled.assert_called_once_with(False)
+        self._view.layout.addWidget.assert_called_once_with(self._view.annot_widget)
+
+    def test_render_annotations(self):
+        dct = {"name": [], "name2": [], "name3": []}
+        self._view._create_annot = MagicMock()
+
+        self._view.annot_list = create_autospec(QListWidget)
+        self._view.annot_list.item(0).sizeHint().height = MagicMock(return_value=10)
+        self._view.render_annotations(dct)
+        self._view._create_annot.assert_has_calls(
+            [mock.call("name", []), mock.call("name2", []), mock.call("name3", [])]
+        )
+        self._view.annot_list.setMaximumHeight.assert_called_once_with(30)
+
+    def test_render_annotations(self):
+        dct = {"name": []}
+        self._view._create_annot = MagicMock()
+
+        self._view.annot_list = create_autospec(QListWidget)
+        self._view.annot_list.item(0).sizeHint().height = MagicMock(return_value=10)
+        self._view.render_annotations(dct)
+        self._view._create_annot.assert_has_calls([mock.call("name", [])])
+        self._view.annot_list.setMaximumHeight.assert_called_once_with(10)
