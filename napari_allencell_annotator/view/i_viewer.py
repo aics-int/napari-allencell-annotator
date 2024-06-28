806f19aaa50c1fbdc60d90d780203ac6ff6460a7
ramita <r.dhamrongsirivadh@alleninstitute.org>
2024-06-28 15:58:42 -0700

    add viewer and iviewer files

---
+++ napari_allencell_annotator/view/i_viewer.py
@@ -0,0 +1,15 @@
+from abc import ABC, abstractmethod
+import numpy as np
+
+class IViewer(ABC):
+    def __init__(self):
+        super().__init__()
+
+    @abstractmethod
+    def add_image(self, image: np.ndarray) -> None:
+        pass
+
+    @abstractmethod
+    def clear_layers(self) -> None:
+        pass
+    
