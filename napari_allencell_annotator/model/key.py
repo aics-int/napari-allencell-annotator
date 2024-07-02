2fd7e6c0e66ff7faf2cd5e4a84c801f7e315710c
ramita <r.dhamrongsirivadh@alleninstitute.org>
2024-07-02 13:30:41 -0700

    resolve conflicts

--- napari_allencell_annotator/model/key.py
+++ napari_allencell_annotator/model/key.py
@@ -2,14 +2,8 @@ from typing import Any
 
 
 class Key:
-<<<<<<< HEAD
-    def __init__(self, key_name: str, type: type, key_default_value: Any = None) -> None:
-        self._key_name = str
-        self._type: type = type
-=======
     def __init__(self, type: str, key_default_value: Any = None) -> None:
         self._type: str = type
->>>>>>> refactor-base
         self._key_default_value = key_default_value
 
     def get_type(self) -> str:
