9a1fc5693a0948984fd83fa59e3de410e244e93a
ramita <r.dhamrongsirivadh@alleninstitute.org>
2024-07-30 14:13:36 -0700

    rename fake layers

--- napari_allencell_annotator/_tests/fakes/fake_viewer.py
+++ napari_allencell_annotator/_tests/fakes/fake_viewer.py
@@ -11,7 +11,7 @@ class FakeViewer(IViewer):
         super().__init__()
 
         self._layers = []
-        self._point_layers = []
+        self._points_layers = []
         self.alerts = []
 
     def add_image(self, image: np.ndarray) -> None:
@@ -27,11 +27,11 @@ class FakeViewer(IViewer):
         return self._layers
 
     def get_all_points_layers(self) -> List[Points]:
-        return self._point_layers
+        return self._points_layers
 
     def create_points_layer(self, name: str, color: str, visible: bool) -> Points:
         points: Points = Points(data=None, name=name, color=color, visible=visible)
-        self._point_layers.append(points)
+        self._points_layers.append(points)
         return points
 
     def get_selected_points(self, point_layer: Points, image_dims_order: str) -> List[Tuple]:
