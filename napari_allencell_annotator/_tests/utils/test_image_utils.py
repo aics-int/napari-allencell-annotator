67343d5d68da648183d7fe0ff3ded119833eaf9f
ramita <r.dhamrongsirivadh@alleninstitute.org>
2024-07-31 12:43:57 -0700

    combine all tests

--- napari_allencell_annotator/_tests/utils/test_image_utils.py
+++ napari_allencell_annotator/_tests/utils/test_image_utils.py
@@ -5,45 +5,13 @@ import pytest
 from napari_allencell_annotator.util.image_utils import ImageUtils
 
 
-def test_get_dask_data_tiff() -> None:
-    # ACT
-    test_image = ImageUtils(
-        Path("napari_allencell_annotator/_tests/assets/image_types/test_img.ome.tiff")
-    ).get_dask_data()
+def test_get_dask_data() -> None:
+    # ARRANGE
+    test_dir: Path = Path("napari_allencell_annotator/_tests/assets/image_types/")
 
-    # ASSERT
-    assert np.all(test_image == np.array([[[0, 0, 0], [0, 0, 0]], [[0, 0, 0], [0, 0, 0]]]))
+    for test_img_path in test_dir.glob("test_img.*"):
+        # ACT
+        test_image = ImageUtils(test_img_path).get_dask_data()
 
-
-def test_get_dask_data_tif() -> None:
-    # ACT
-    test_image = ImageUtils(
-        Path("napari_allencell_annotator/_tests/assets/image_types/test_img.ome.tif")
-    ).get_dask_data()
-
-    # ASSERT
-    assert np.all(test_image == np.array([[[0, 0, 0], [0, 0, 0]], [[0, 0, 0], [0, 0, 0]]]))
-
-
-def test_get_dask_data_png() -> None:
-    # ACT
-    test_image = ImageUtils(Path("napari_allencell_annotator/_tests/assets/image_types/test_img.png")).get_dask_data()
-
-    # ASSERT
-    assert np.all(test_image == np.array([[[0, 0, 0], [0, 0, 0]], [[0, 0, 0], [0, 0, 0]]]))
-
-
-def test_get_dask_data_jpg() -> None:
-    # ACT
-    test_image = ImageUtils(Path("napari_allencell_annotator/_tests/assets/image_types/test_img.jpg")).get_dask_data()
-
-    # ASSERT
-    assert np.all(test_image == np.array([[[0, 0, 0], [0, 0, 0]], [[0, 0, 0], [0, 0, 0]]]))
-
-
-def test_get_dask_data_jpeg() -> None:
-    # ACT
-    test_image = ImageUtils(Path("napari_allencell_annotator/_tests/assets/image_types/test_img.jpeg")).get_dask_data()
-
-    # ASSERT
-    assert np.all(test_image == np.array([[[0, 0, 0], [0, 0, 0]], [[0, 0, 0], [0, 0, 0]]]))
+        # ASSERT
+        assert np.all(test_image == np.array([[[0, 0, 0], [0, 0, 0]], [[0, 0, 0], [0, 0, 0]]]))
