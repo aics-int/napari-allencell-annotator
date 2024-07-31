b666ff6d2f7a088d2d3eeca4517bac880746997d
ramita <r.dhamrongsirivadh@alleninstitute.org>
2024-07-31 12:09:51 -0700

    test png jpeg jpg

--- napari_allencell_annotator/_tests/utils/test_image_utils.py
+++ napari_allencell_annotator/_tests/utils/test_image_utils.py
@@ -7,7 +7,9 @@ from napari_allencell_annotator.util.image_utils import ImageUtils
 
 def test_get_dask_data_tiff() -> None:
     # ACT
-    test_image = ImageUtils(Path("napari_allencell_annotator/_tests/assets/image_types/test_img.ome.tiff")).get_dask_data()
+    test_image = ImageUtils(
+        Path("napari_allencell_annotator/_tests/assets/image_types/test_img.ome.tiff")
+    ).get_dask_data()
 
     # ASSERT
     assert (test_image == np.array([[[0, 0, 0], [0, 0, 0]], [[0, 0, 0], [0, 0, 0]]])).all()
@@ -15,8 +17,33 @@ def test_get_dask_data_tiff() -> None:
 
 def test_get_dask_data_tif() -> None:
     # ACT
-    test_image = ImageUtils(Path("napari_allencell_annotator/_tests/assets/image_types/test_img.ome.tif")).get_dask_data()
+    test_image = ImageUtils(
+        Path("napari_allencell_annotator/_tests/assets/image_types/test_img.ome.tif")
+    ).get_dask_data()
 
     # ASSERT
     assert (test_image == np.array([[[0, 0, 0], [0, 0, 0]], [[0, 0, 0], [0, 0, 0]]])).all()
 
+
+def test_get_dask_data_png() -> None:
+    # ACT
+    test_image = ImageUtils(Path("napari_allencell_annotator/_tests/assets/image_types/test_img.png")).get_dask_data()
+
+    # ASSERT
+    assert (test_image == np.array([[[0, 0, 0], [0, 0, 0]], [[0, 0, 0], [0, 0, 0]]])).all()
+
+
+def test_get_dask_data_jpg() -> None:
+    # ACT
+    test_image = ImageUtils(Path("napari_allencell_annotator/_tests/assets/image_types/test_img.jpg")).get_dask_data()
+
+    # ASSERT
+    assert (test_image == np.array([[[0, 0, 0], [0, 0, 0]], [[0, 0, 0], [0, 0, 0]]])).all()
+
+
+def test_get_dask_data_jpeg() -> None:
+    # ACT
+    test_image = ImageUtils(Path("napari_allencell_annotator/_tests/assets/image_types/test_img.jpeg")).get_dask_data()
+
+    # ASSERT
+    assert (test_image == np.array([[[0, 0, 0], [0, 0, 0]], [[0, 0, 0], [0, 0, 0]]])).all()
