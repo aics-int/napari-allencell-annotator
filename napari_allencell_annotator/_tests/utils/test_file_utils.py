9371be454c487d9d66486ee2d64ac00fafaa7dd0
ramita <r.dhamrongsirivadh@alleninstitute.org>
2024-08-02 13:03:09 -0700

    add valid files test

--- napari_allencell_annotator/_tests/utils/test_file_utils.py
+++ napari_allencell_annotator/_tests/utils/test_file_utils.py
@@ -1,9 +1,38 @@
 from pathlib import Path
 
 from napari_allencell_annotator.util.file_utils import FileUtils
+import napari_allencell_annotator
 
 
-def test_get_file_name():
+def test_select_only_valid_files() -> None:
+    # ARRANGE
+    all_files = [
+        Path("test.tiff"),
+        Path("test.png"),
+        Path("test.jpeg"),
+        Path("test.czi"),
+        Path("test/raw.ome.zarr"),
+        Path("test.csv"),
+        Path("test.json"),
+        Path("valid_img_dir"),
+        Path(napari_allencell_annotator.__file__).parent / "_tests" / "assets" / "zarr_dir",
+    ]
+
+    # ACT
+    valid_files = FileUtils.select_valid_images(all_files)
+
+    # ASSERT
+    assert valid_files == [
+        Path("test.tiff"),
+        Path("test.png"),
+        Path("test.jpeg"),
+        Path("test.czi"),
+        Path("test/raw.ome.zarr"),
+        Path(napari_allencell_annotator.__file__).parent / "_tests" / "assets" / "zarr_dir" / "raw.ome.zarr",
+    ]
+
+
+def test_get_file_name() -> None:
     # ACT
     tiff_file_name: str = FileUtils.get_file_name(Path("parent/test.tiff"))
     png_file_name: str = FileUtils.get_file_name(Path("parent/test.png"))
@@ -17,4 +46,3 @@ def test_get_file_name():
     assert jpeg_file_name == "test.jpeg"
     assert czi_file_name == "test.czi"
     assert zarr_file_name == "parent"
-
