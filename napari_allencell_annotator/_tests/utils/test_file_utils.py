641365798cb727577d5974d3f4d68b3fe0303201
ramita <r.dhamrongsirivadh@alleninstitute.org>
2024-08-05 13:05:00 -0700

    add test back

--- napari_allencell_annotator/_tests/utils/test_file_utils.py
+++ napari_allencell_annotator/_tests/utils/test_file_utils.py
@@ -36,8 +36,14 @@ def test_get_valid_images_sorted() -> None:
 def test_get_file_name() -> None:
     # ACT
     tiff_file_name: str = FileUtils.get_file_name(Path("parent/test.tiff"))
+    png_file_name: str = FileUtils.get_file_name(Path("parent/test.png"))
+    jpeg_file_name: str = FileUtils.get_file_name(Path("parent/test.jpeg"))
+    czi_file_name: str = FileUtils.get_file_name(Path("parent/test.czi"))
     zarr_file_name: str = FileUtils.get_file_name(Path("parent/test.ome.zarr"))
 
     # ASSERT
     assert tiff_file_name == "test.tiff"
+    assert png_file_name == "test.png"
+    assert jpeg_file_name == "test.jpeg"
+    assert czi_file_name == "test.czi"
     assert zarr_file_name == "parent"
