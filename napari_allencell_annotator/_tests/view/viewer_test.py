c1c122bf592138a2631dc224c385811ddcccbb04
ramita <r.dhamrongsirivadh@alleninstitute.org>
2024-08-05 16:04:04 -0700

    tests

--- napari_allencell_annotator/_tests/view/viewer_test.py
+++ napari_allencell_annotator/_tests/view/viewer_test.py
@@ -4,15 +4,40 @@ from napari_allencell_annotator.view.viewer import Viewer, PointsLayerMode
 import napari
 import numpy as np
 import pytest
+from napari.utils import Colormap
+
 
 @pytest.fixture
 def viewer() -> Viewer:
     return Viewer(napari.Viewer(show=False))
 
 
+def test_create_points_layer(viewer: Viewer) -> None:
+    # ACT
+    points_layer: Points = viewer.create_points_layer("test", "blue", True, 2)
+
+    # ASSERT
+    assert points_layer in viewer.get_all_points_layers()
+    assert len(viewer.get_all_points_layers()) == 1
+    assert points_layer.name == "test"
+    assert points_layer.visible
+    assert points_layer.ndim == 2
+    assert len(points_layer.data) == 0
+    assert points_layer.mode == PointsLayerMode.ADD.value
+
+
+def test_create_points_layer_color(viewer: Viewer) -> None:
+    # ACT
+    points_layer: Points = viewer.create_points_layer("test", "blue", True, 2)
+    points_layer.data = np.array([[1, 1]])
+
+    # ASSERT
+    assert np.array_equal(points_layer.face_color[0], Colormap("blue").colors[0])
+
+
 def test_set_points_layer_mode(viewer: Viewer) -> None:
     # ARRANGE
-    points_layer: Points = viewer.viewer.add_points(None)
+    points_layer: Points = viewer.create_points_layer("test", "blue", True, 2)
 
     # ACT
     viewer.set_points_layer_mode(points_layer, PointsLayerMode.ADD)
@@ -23,8 +48,9 @@ def test_set_points_layer_mode(viewer: Viewer) -> None:
 
 def test_get_selected_points(viewer: Viewer) -> None:
     # ARRANGE
+    points_layer: Points = viewer.create_points_layer("test", "blue", True, 2)
     points: np.array = np.array([[1, 2], [3, 4]])
-    points_layer: Points = viewer.viewer.add_points(points)
+    points_layer.data = points
 
     # ACT
     selected_points: list[tuple] = viewer.get_selected_points(points_layer)
@@ -35,11 +61,14 @@ def test_get_selected_points(viewer: Viewer) -> None:
 
 def test_get_all_point_annotations(viewer: Viewer) -> None:
     # ARRANGE
+    points_layer1: Points = viewer.create_points_layer("test1", "blue", True, 2)
+    points_layer2: Points = viewer.create_points_layer("test2", "blue", True, 2)
+
     points1: np.array = np.array([[1, 2], [3, 4]])
     points2: np.array = np.array([[5, 6], [7, 8]])
 
-    viewer.viewer.add_points(points1, name="test1")
-    viewer.viewer.add_points(points2, name="test2")
+    points_layer1.data = points1
+    points_layer2.data = points2
 
     # ACT
     all_point_annotations: dict[str, list[tuple]] = viewer.get_all_point_annotations()
