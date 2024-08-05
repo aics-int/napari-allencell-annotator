8684f8b93df8f83bc3413fce03418047313eb897
ramita <r.dhamrongsirivadh@alleninstitute.org>
2024-08-05 16:55:25 -0700

    fix get image test

--- napari_allencell_annotator/_tests/utils/test_image_utils.py
+++ napari_allencell_annotator/_tests/utils/test_image_utils.py
@@ -1,18 +1,19 @@
 from pathlib import Path
 import numpy as np
+from bioio import BioImage
 
 import napari_allencell_annotator
 from napari_allencell_annotator.util.image_utils import ImageUtils
 
 
-def test_get_dask_data_tiff() -> None:
+def test_get_image_tiff() -> None:
     # ARRANGE
     test_path: Path = (
         Path(napari_allencell_annotator.__file__).parent / "_tests" / "assets" / "image_types" / "img.ome.tiff"
     )
 
     # ACT
-    test_image = ImageUtils(test_path).get_dask_data()
+    test_image: BioImage = ImageUtils(test_path).get_image()
 
     # ASSERT
-    np.testing.assert_array_equal(test_image[0, 0, :, :, :, :], np.zeros((2, 2, 2, 2)))
+    np.testing.assert_array_equal(test_image.get_dask_stack()[0, 0, :, :, :, :], np.zeros((2, 2, 2, 2)))
