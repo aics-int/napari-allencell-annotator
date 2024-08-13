703748b117f60df493ef02bd1ec0fbba5389dcfd
brian.kim <yrkim98@uw.edu>
2024-08-13 15:33:23 -0700

    fix annotator test- use qtbot

--- napari_allencell_annotator/_tests/controller/test_annotator_controller.py
+++ napari_allencell_annotator/_tests/controller/test_annotator_controller.py
@@ -1,32 +1,19 @@
-import pytest
 from napari_allencell_annotator._tests.fakes.fake_viewer import FakeViewer
 from napari_allencell_annotator.view.annotator_view import AnnotatorViewMode
-
-from napari_allencell_annotator.view.i_viewer import IViewer
-
-from napari_allencell_annotator.model.annotation_model import AnnotatorModel
-
 from napari_allencell_annotator.controller.annotator_controller import AnnotatorController
+from napari_allencell_annotator.view.main_view import MainView
+
+import pytest
+from pytestqt import qtbot
 
 
-@pytest.fixture
-def annotator_model(qtbot) -> AnnotatorModel:
-    return AnnotatorModel()
 
+def test_start_annotating(qtbot) -> None:
+    # ARRANGE
+    main_view_simulated: MainView = MainView(FakeViewer())
+    qtbot.add_widget(main_view_simulated)
+    annotator_controller: AnnotatorController = main_view_simulated.annots
 
-@pytest.fixture
-def viewer(qtbot) -> IViewer:
-    return FakeViewer()
-
-
-@pytest.fixture
-def annotator_controller(qtbot, annotator_model: AnnotatorModel, viewer: IViewer) -> AnnotatorController:
-    annotator_controller_fixture = AnnotatorController(annotator_model, viewer)
-    annotator_controller_fixture.view.hide()
-    return annotator_controller_fixture
-
-
-def test_start_annotating(annotator_controller: AnnotatorController) -> None:
     # ACT
     annotator_controller.start_annotating()
 
